f372b3b42f4c40db7a280d01aac64ab8bb1446ad
蛍 <93361361+minamikana-git@users.noreply.github.com>
2023-05-24 15:07:04 +0900

    first commit

--- src/main/java/utakatanet/work/hotelreservation/Hotel.java
+++ src/main/java/utakatanet/work/hotelreservation/Hotel.java
@@ -1,2 +1,47 @@
-package utakatanet.work.hotelreservation;public class Hotel {
+package utakatanet.work.hotelreservation;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+public class Hotel {
+    private List<HotelReservation> reservations;
+    private List<HotelRoom> rooms;
+
+    public Hotel() {
+        reservations = new ArrayList<>();
+        rooms = new ArrayList<>();
+    }
+
+    public void addReservation(HotelReservation reservation) {
+        reservations.add(reservation);
+    }
+
+    public void addRoom(HotelRoom room) {
+        rooms.add(room);
+    }
+
+    public List<HotelReservation> getReservations() {
+        return reservations;
+    }
+
+    public List<HotelRoom> getRooms() {
+        return rooms;
+    }
+
+    public boolean isRoomAvailable(int roomNumber, Date checkInDate, Date checkOutDate) {
+        for (HotelReservation reservation : reservations) {
+            if (reservation.getRoomNumber() == roomNumber) {
+                if ((checkInDate.after(reservation.getCheckInDate()) && checkInDate.before(reservation.getCheckOutDate())) ||
+                        (checkOutDate.after(reservation.getCheckInDate())
+                                && checkOutDate.before(reservation.getCheckOutDate()))) {
+                    return false;
+                }
+            }
+        }
+        for (HotelRoom room : rooms) {
+            if (room.getRoomNumber() == roomNumber) {
+                return room.isAvailable();
+            }
+        }
+        return false;
+    }
 }
--- src/main/java/utakatanet/work/hotelreservation/HotelRoom.java
+++ src/main/java/utakatanet/work/hotelreservation/HotelRoom.java
@@ -1,2 +1,39 @@
-package utakatanet.work.hotelreservation;public class HotelRoom {
+package utakatanet.work.hotelreservation;
+
+public class HotelRoom {
+    private int roomNumber;
+    private int price;
+    private boolean isAvailable;
+
+    public HotelRoom(int roomNumber, int price, boolean isAvailable) {
+        this.roomNumber = roomNumber;
+        this.price = price;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+}
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+    private int roomNumber;
+    private boolean isAvailable;
+
+    // コンストラクタとその他のメソッド
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+    // getter and setter methods
 }
